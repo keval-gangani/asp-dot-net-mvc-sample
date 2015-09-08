64504eb314ef10c4eda7500afe63399e1762518d
Keval Gangani <keval.gangani@gmail.com>
2015-09-08 14:53:05 +0530

    First Commit

---
+++ ASP-Dot-Net-MVC-Sample/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_Dot_Net_MVC_Sample.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString() 
+        {
+
+            return "Hello World;";
+        }
+
+	}
+}
---
+++ ASP-Dot-Net-MVC-Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP_Dot_Net_MVC_Sample.MvcApplication" Language="C#" %>
